9c0981d881d4eada3098724acdbddedeb758dd86
shirsath06ashwini-ux <shirsath06ashwini@gmail.com>
2025-09-03 20:31:02 +0530

    Add files via upload

---
+++ import java.util.java
@@ -0,0 +1,13 @@
+import java.util.random;
+import java.util.*;
+
+public class guess the number
+{
+    public static void main(String args[])
+{
+    Scanner scanner=new Scanner(System.in);
+    random random=newrandom();
+
+    //generate a random number between 1 and 100
+    }
+}
